1cb97abd960f408a69bff4043f5e2546ba568ea2
eng-omar23 <omarmohamud531@gmail.com>
2023-08-30 14:46:39 +0300

    approve and reject buutom working

--- application/views/admin/adminnotification.php
+++ application/views/admin/adminnotification.php
@@ -20,9 +20,10 @@
 
     $sql = "SELECT * FROM bookings b
     LEFT JOIN customers c ON b.customer_id = c.custid
-    LEFT JOIN transactions tr ON tr.custid = b.customer_id
     LEFT JOIN halls h ON h.hall_id = b.hall_id
-    WHERE booking_status = 0
+    LEFT JOIN transactions tr ON tr.refID = b.hall_id
+
+    WHERE booking_status = 0  
     ORDER BY b.booking_id DESC";
     $query = mysqli_query($conn, $sql);
     ?>
@@ -59,24 +60,38 @@
                                                 while ($row = mysqli_fetch_assoc($query)) {
                                                     // Extract relevant data
                                                     $customer = $row['firstname'];
+                                                    $bid = $row['booking_id'];
                                                     $hall = $row['hall_type'];
+                                                   
                                                     $credit = $row['credit'];
+
                                                     $debit = $row['debit'];
                                                     $date = date('Y-m-d', strtotime($row['created_at']));
                                                     $startTime = $row['starttime'];
                                                     $endTime = $row['endtime'];
                                                     $balance = $row['debit']-$row['credit'];
                                                     $bookingStatus = $row['booking_status'];
-
+                                              
+                            
                                                     echo "<tr>";
                                                     echo "<td>" . $customer . "</td>";
                                                     echo "<td>" . $hall . "</td>";
-                                                    echo "<td>" . $credit . "</td>";
-                                                    echo "<td>" . $debit . "</td>";
-                                                    echo "<td>" . $balance . "</td>";
+                                                    if ($credit == 0) {
+                                                        echo "<td>0$</td>";
+                                                      } else {
+                                                        echo "<td>{$credit}$</td>";
+                                                      }
+                                                      if ($debit == 0) {
+                                                        echo "<td>0$</td>";
+                                                      } else {
+                                                        echo "<td>{$debit}$</td>";
+                                                      }
+                                            
                                                     echo "<td>" . $date . "</td>";
+                                           
                                                     echo "<td>" . $startTime . "</td>";
                                                     echo "<td>" . $endTime . "</td>";
+                                                    echo "<td>" . $balance . "</td>";
                                                   
                                                     echo "<td>";
                                                     if ($bookingStatus == 0) {
@@ -84,15 +99,12 @@
                                                     }
                                                     echo "</td>";
                                                     echo "<td>
-                                                            <a href='#' class='text-success p-2 approve-btn' 
-                                                               data-id='" . $row['booking_id'] . "'>
-                                                               Approve
-                                                           </a>
-                                                           <a href='#' class='text-danger p-2 reject-btn' 
-                                                              data-id='" . $row['booking_id'] . "'>
-                                                              Reject
-                                                          </a>
-                                                          </td>";
+                                                    <li class='list-inline-item'>
+                                                    <a href='#' class='text-success p-2 approve-btn'  data-id='$bid'><i class='fas fa-check-circle'></i></a>
+                                                    </li>
+                                                    <li class='list-inline-item'>
+                                                    <a href='#' class='text-danger p-2 reject-btn' data-id='$bid'> <i class='fas fa-times-circle'></i></a>
+                                                </li></td>";
                                                     echo "</tr>";
                                                 }
                                             } else {
@@ -109,42 +121,63 @@
             </div>
         </div>
     </center>
-
-    <script>
-    // JavaScript/jQuery code for handling approve and reject buttons
-    $(document).ready(function() {
-        $('.approve-btn').on('click', function(event) {
-            event.preventDefault();
-            var bookingId = $(this).data('id');
-            updateBookingStatus(bookingId, 'approve');
-        });
-
-        $('.reject-btn').on('click', function(event) {
-            event.preventDefault();
-            var bookingId = $(this).data('id');
-            updateBookingStatus(bookingId, 'reject');
-        });
-
-        function updateBookingStatus(bookingId, status) {
-            $.ajax({
-                url: 'update_booking_status.php', // Replace with correct URL
-                method: 'POST',
-                data: {
-                    bookingId: bookingId,
-                    bookingStatus: status
-                },
-                success: function(response) {
-                    // Handle the response if needed
-                    window.location.href = "adminnotification.php";
-                },
-                error: function(xhr, status, error) {
-                    // Handle errors if any
-                    console.error(error);
-                }
-            });
-        }
-    });
-    </script>
-</body>
-
+<!-- Add Bootstrap JS (Optional) -->
+    <!-- You can include it at the end of the body tag if needed -->
+    <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.min.js"></script>  -->
+    </body>
 </html>
+<!-- Include jQuery library -->
+<!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
+
+
+<!-- JavaScript/jQuery code -->
+<script>
+    // Function to handle the "Approve" button click
+    $('.approve-btn').on('click', function (event) {
+        event.preventDefault();
+        var aproveid = $(this).data('id');
+       
+        // Send AJAX request to update the booking_status
+        $.ajax({
+            url: 'update_booking_status.php', // Replace with your PHP script URL
+            method: 'POST',
+            data: {
+                aproveid: aproveid,
+                booking_status: 'approve', // Change to 'rejected' for the Reject button
+              
+            },
+            success: function (response) {
+                // Handle the response if needed
+                window.location.href = "adminnotification.php";
+            },
+            error: function (xhr, status, error) {
+                // Handle errors if any
+                console.error(error);
+            }
+        });
+    });
+
+    // Function to handle the "Reject" button click
+    $('.reject-btn').on('click', function (event) {
+        event.preventDefault();
+        var rid = $(this).data('id');
+
+        // Send AJAX request to update the booking_status
+        $.ajax({
+            url: 'update_booking_status.php', // Replace with your PHP script URL
+            method: 'POST',
+            data: {
+                rid: rid,
+                booking_status: 'reject',// Change to 'approved' for the Approve button
+            },
+            success: function (response) {
+                // Handle the response if needed
+                window.location.href = "adminnotification.php";
+            },
+            error: function (xhr, status, error) {
+                // Handle errors if any
+                console.error(error);
+            }
+        });
+    });
+</script>
